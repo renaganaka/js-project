1a4b40a410b370fc8bdfd22d2347a56dcc7955eb
renaganaka <riglfeed@gmail.com>
2025-05-14 12:04:53 +0600

    added converter

--- js/lesson.js
+++ js/lesson.js
@@ -36,3 +36,49 @@ tabItems.forEach((item, index) => {
 });
 
 
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const euroInput = document.querySelector('#euro');
+
+const converter = (element) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open('GET', '../data/converter.json');
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send();
+
+        request.onload = () => {
+            const data = JSON.parse(request.response);
+
+            if (element.value === '') {
+                somInput.value = '';
+                usdInput.value = '';
+                euroInput.value = '';
+                return;
+            }
+
+            if (element.id === 'som') {
+                usdInput.value = (element.value / data.usd).toFixed(2);
+                euroInput.value = (element.value / data.euro).toFixed(2);
+            }
+
+            if (element.id === 'usd') {
+                somInput.value = (element.value * data.usd).toFixed(2);
+                euroInput.value = ((element.value * data.usd) / data.euro).toFixed(2);
+            }
+
+            if (element.id === 'euro') {
+                somInput.value = (element.value * data.euro).toFixed(2);
+                usdInput.value = ((element.value * data.euro) / data.usd).toFixed(2);
+            }
+        };
+    };
+};
+
+converter(somInput);
+converter(usdInput);
+converter(euroInput);
+
+
+
+
